b10541a404f43dd5e4e455d329b729d5037ffdc5
David <david.melo@lc.cuny.edu>
2023-05-15 18:04:50 -0400

    added more to index

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
 const PORT = process.env.PORT || 5432;
+
+app.listen (5432);
