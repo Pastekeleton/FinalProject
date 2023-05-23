bdbf10d78f3d6092634f0dacb62602ae8f2f803a
David <david.melo@lc.cuny.edu>
2023-05-23 14:22:18 -0400

    Tried my best but finished!

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,14 @@ import { Login } from "./Login";
 import { Register } from "./Register";
 
 function App() {
- /* const [data, setData] = React.useState(null);
+ const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
-  */
+  
  const [currentForm, setCurrentForm] = useState('login');
  const toggleForm = (formName) => {
   setCurrentForm(formName);
